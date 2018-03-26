43262a2ebb464f9ed925798c345fcf21564e76fa
Your Name <you@example.com>
2018-03-26 23:14:31 +0800

    add new files

--- chapter2/Exercise8.java
+++ chapter2/Exercise8.java
@@ -1,25 +1,28 @@
+// write a program demonstrates that, no matter how many objects you create of a particular class
+// there is only one instance of a particular static field of that class.
+
 // Turn the Incrementable code fragments into a working program.
 
-class StaticTest {
+class StaticTest1 {
     static int i = 47;
 }
 
-class Incrementable {
+class Incrementable1 {
     static void increment() { StaticTest.i++; }
 }
-class Exercise7 {
+class Exercise8 {
     public static void main(String[] args) {
-        System.out.println("StaticTest.i= " + StaticTest.i);
-        StaticTest st1 = new StaticTest();
-        StaticTest st2 = new StaticTest();
+        System.out.println("StaticTest.i= " + StaticTest1.i);
+        StaticTest1 st1 = new StaticTest1();
+        StaticTest1 st2 = new StaticTest1();
         System.out.println("st1.i= " + st1.i);
         System.out.println("st2.i= " + st2.i);
-        Incrementable sf = new Incrementable();
+        Incrementable1 sf = new Incrementable1();
         sf.increment();
         System.out.println("After sf.increment() called: ");
         System.out.println("st1.i = " + st1.i);
         System.out.println("st2.i = " + st2.i);
-        Incrementable.increment();
+        Incrementable1.increment();
         System.out.println("After Incrementable.increment called: ");
         System.out.println("st1.i = " + st1.i);
         System.out.println("st2.i = " + st2.i);
