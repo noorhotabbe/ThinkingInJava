a44ed8abfd8dc05d8b66c564db638782dafbee30
Your Name <you@example.com>
2018-04-07 00:05:14 +0800

    add some files

---
+++ chapter4/Exercise1.java
@@ -0,0 +1,11 @@
+// Write a program that prints values from 1 to 100
+
+public class Exercise1{
+	public static void main(String args[])
+	{
+		for(int i = 0; i < 100; i++)
+		{
+			System.out.println("i:"+i);
+		}
+	}
+}
---
+++ chapter4/Exercise2.java
@@ -0,0 +1,4 @@
+/*Write a program that generates  25 random int values, 
+for each value, use an if-else statement to classify it as
+greater than, less than, or equal to a second random generated value.
+*/
